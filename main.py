d1a9806d4f55ed046f27f67c3c1709f42dc0cc2b
Jacob Thomas <thomajac002@warren.k12.in.us>
2024-08-27 10:03:37 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,7 @@
+miles = 3 * 1
+minutes = 30 * 2
+hours = minutes / 60
+mph = miles / hours
+print("How many miles did you run?", miles)
+print("How many minutes did it take you?", minutes)
+print("Speed in mph ", mph)
